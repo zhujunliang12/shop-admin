6071403a4393fc029b1164cf9f02364315459b26
zhujunliang <260006856@qq.com>
2020-08-09 20:52:41 +0800

    add statud

---
+++ src/main/java/com/fh/student/Book.java
@@ -0,0 +1,14 @@
+package com.fh.student;
+
+/**
+ * @ClassName : Book  //类名
+ * @Description : book  //描述
+ * @Author : 朱俊亮  //作者
+ * @Date: 2020-08-09 20:42  //时间
+ */
+public class Book {
+
+    public static void main(String[] args) {
+        System.out.println("=========");
+    }
+}
